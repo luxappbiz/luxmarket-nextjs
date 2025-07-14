d2d5996c99171bc80622dc3e4aaf61240c8863de
Abdul Rehman <abdulrehmanali82@gmail.com>
2025-07-14 21:19:40 +0500

    Middelware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("auth_token")?.value;
+  // console.log('middleware')
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next|favicon.ico|assets|login|join|lost-pass|explore|$).*)", // Exclude these paths
+  ],
+};
