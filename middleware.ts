226c78e5ccc6dd6ae47867baa6530534e3753336
Abdul Rehman <abdulrehmanali82@gmail.com>
2025-08-28 00:30:50 +0500

    middelware change

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next|favicon.ico|assets|login|join|lost-pass|blog|support|docs|knowledge|platform|privacy|terms|$).*)", // Exclude these paths
+    "/((?!_next|favicon.ico|assets|images|_next/image|login|join|lost-pass|blog|support|docs|knowledge|platform|privacy|terms|$).*)", // Exclude these paths
   ],
 };
